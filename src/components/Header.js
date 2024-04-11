32ef4de131446c875c5f3c50f9f972b57ee77c9b
Joshua Lazar <interlink@192.168.1.25>
2024-04-11 12:23:33 -0700

    firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div>
-      <h1>Hello World developments</h1>
+      <h1>Hello World developments firebase</h1>
     </div>
   );
 }
